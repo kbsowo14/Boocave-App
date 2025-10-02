0b93fba8902c135ab66b40cb98a58ab9b8ead008
Nongeoking <kbsowo14@naver.com>
2025-10-02 15:49:16 +0900

    Update Indicator

--- App.tsx
+++ App.tsx
@@ -24,7 +24,7 @@ export default function App() {
 					startInLoadingState={true}
 					renderLoading={() => (
 						<View style={styles.loadingContainer}>
-							<ActivityIndicator size="large" color="#0000ff" />
+							<ActivityIndicator size="small" color="#51CD42" />
 						</View>
 					)}
 					// JavaScript 활성화
